35a56aa1707611cefec71796ba066859ff6b5093
elainatiller <55641497+elainatiller@users.noreply.github.com>
2021-03-24 15:26:23 -0400

    Add files via upload

---
+++ include.js
@@ -0,0 +1,16 @@
+
+$(function(){
+    $("#includeAbout").load("about.html"); 
+});
+$(function(){
+    $("#includeResume").load("resume.html"); 
+});
+$(function(){
+    $("#includeWork").load("work.html"); 
+  });
+$(function(){
+    $("#includeContact").load("contact.html"); 
+});
+$(function(){
+    $("#includePhotography").load("photography.html"); 
+});
